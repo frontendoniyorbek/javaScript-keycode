07cf785ff9da925f65e9fc3443ecd783cbfca25c
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2024-07-24 14:33:59 +0500

    add altKey

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const elEventTitle = document.querySelector('.event__title');
 const elEventLocationTitle = document.querySelector('.event__location');
 const elEventCode = document.querySelector('.event__code');
 const elEventWhich = document.querySelector('.event__which');
+const elAltKey = document.querySelector('.event__alltKey');
 
 window.addEventListener('keyup', event => {
 	if (event.isTrusted === true) {
@@ -23,6 +24,11 @@ window.addEventListener('keyup', event => {
 
 	elEventCode.textContent = event.code;
 	elEventWhich.textContent = event.which;
+	if (event.code === 'Space') {
+		elAltKey.textContent = 'Space';
+	} else {
+		elAltKey.textContent = event.altKey;
+	}
 
 	console.log(event);
 });
